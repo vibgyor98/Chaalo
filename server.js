ce28559ff7f2d0b2fc30d44d9f5da54e8ad925f6
Sourav Kar <karsourav05@gmail.com>
2020-04-20 22:47:02 +0530

    ++ mongodb

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config({ path: './config.env'}) //first chk the env
 
 //then proceed to app
 const app = require('./app');
+
+//connecting database
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(con => {
+    console.log('DB Connection Successful');
+});
+
+//mongoose schema
+const tourSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+//Models
+const Tour = mongoose.model('Tour', tourSchema);
 
 //connecting server
 const port = process.env.PORT || 3000;
